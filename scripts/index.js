d03ef97e449b43d29acd9671f083f4dc7bad56b4
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2021-01-08 15:39:59 +0100

    Général(MAJ): Ajout de la fonctionnalité permettant de deviner directement le mot. Modifs faites dans l'HTML puis index.js.

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,27 @@ InputLettre.forEach(value => {
         VerifierStage(ExisteDansLeMot);
     });
 });
+
+const DevinerMotEntier = document.querySelector("input.MotEntier");
+
+DevinerMotEntier.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        if (DevinerMotEntier.value !== ""){
+            if(DevinerMotEntier.value.toUpperCase() === RegistreDuMot.Mot){
+                DevinerMotEntier.style.color = "green";
+                RegistreDuMot.MotDecoupe.forEach(LettreDuMot =>{
+                    let HiddenLetter = document.querySelector(".InterMotADeviner .Mot .\\3" + LettreDuMot.Class[0] + (LettreDuMot.Class[1] === undefined ? "" : " " + LettreDuMot.Class[1]));
+                    HiddenLetter.innerHTML = LettreDuMot.Lettre;
+                    LettreDuMot.EstDecouverte = true;
+                    HiddenLetter.style.color = "green";
+                });
+                VerifierSiMotComplet();
+            }else{
+                DevinerMotEntier.style.color = "red";
+                VerifierStage(false)
+            }
+        }
+    }
+
+});

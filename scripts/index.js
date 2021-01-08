a652c2e6a896d5e17300c8835c580b39084fe790
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2021-01-08 14:56:53 +0100

    Scripts(MAJ): Epuration de l'index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,17 @@
 genererMot();
-
-
-
-console.log(ClavierDeLettres);
-
-
-let elem = document.querySelector(".ClavierDeLettres");
-console.log(elem);
-
 genererClavier();
 
-
-// Mettre tout les addEventListenner ici
-
-
-//Je clique sur une lettre
-// Je parcours le mot et je vérifie si une lettre existe
-// si une lettre existe, booléen a true et je fais découvrir la lettre en mettant EstDecouvert a true
-// je continue jusqu'a la fin du parcours
-
-const InputLettre = document.querySelectorAll("input.LettreClavier")
-
+const InputLettre = document.querySelectorAll("input.LettreClavier");
 InputLettre.forEach(value => {
     value.addEventListener("click", function(e){
-       let LettreSelectionnee = e.target.defaultValue;
-       let ExisteDansLeMot = false;
-       RegistreDuMot.MotDecoupe.forEach(LettreDuMot =>{
-          if (LettreSelectionnee === LettreDuMot.Lettre){
-              ExisteDansLeMot = true;
-              LettreDuMot.EstDecouverte = true;
-          }
-       });
-
-
-
+       let ExisteDansLeMot = VerifierLettre(e);
+       console.log(ExisteDansLeMot);
+       if (ExisteDansLeMot){
+           BonnePioche("Lettre", e);
+           VerifierSiMotComplet();
+       } else {
+           MauvaisePioche("Lettre", e)
+       }
+        VerifierStage(ExisteDansLeMot);
     });
 });
